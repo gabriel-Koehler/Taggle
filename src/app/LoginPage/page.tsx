6ecbc675e77650b8dd94f5c484f31b24805e2c3c
gabriel-Koehler <ggaabbrriieellks@gmail.com>
2024-12-19 10:37:02 -0300

    init project

---
+++ src/app/LoginPage/page.tsx
@@ -0,0 +1,6 @@
+export default function loginPage() {
+  return (<>
+    <h1>Legal</h1>
+    <h1>jskladsjka</h1>
+    </>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import loginPage from "./LoginPage/page";
 
 export default function Home() {
   return (
@@ -95,6 +97,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Link href="/LoginPage" >login</Link>
       </footer>
     </div>
   );
